90ee25dbb51cd34f2b663d1bf56c721bfde2e675
Daniel <daniel_l0l@hotmail.com>
2021-10-24 13:23:09 -0300

    add responsividade

---
+++ main.js
@@ -0,0 +1,35 @@
+const LinksSocialMedia = {
+  github: 'daniell0l',
+  youtube: 'darkzinho155',
+  facebook: 'profile.php?id=100003891436040',
+  instagram: 'daniel_medrado.f',
+  twitter: 'daniel medrado'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+
+    //alert(li.children[0].href)
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(Response => Response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      UserImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
